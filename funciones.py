ecdbd35b4ecca9d680e3e80c03445abef49d0503
sebas-777 <sebastiancast06@hotmail.com>
2022-05-09 08:56:18 -0500

    segunda seccion

---
+++ funciones.py
@@ -0,0 +1,17 @@
+# nombreFuncion(   parametro1 ,parametro2 .....Nparametros )
+
+# def nombrefuncion( parametro1 ,parametro2...nparametros):
+# return 
+
+# 1 una funcion que calcule la exponenciacion  de dos numeros 
+
+def calcularExponenciacion(base , exponente ) :
+    print(base ** exponente)
+    return
+
+
+# 12**11  para llamar una funcion  y sus parametros 
+
+
+calcularExponenciacion(19,11)
+
